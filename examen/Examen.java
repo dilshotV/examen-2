47458c55ed323effde1a42ba7414fce68caf2e7f
dilshotV <dilshotvargas@gmail.com>
2025-11-05 19:13:32 -0600

    Add files via upload

---
+++ examen/Examen.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen;
+import examen.Facturas.Factura;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author dilsh
+ */
+public class Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+    
+    }
+}
+       
+  
+  
+      
+        
+   
+       
+
+     
+        
+
+        
+        
+        
+    
+
+    
